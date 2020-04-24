70f7aacf13fe18e83e27f548eccfb41b354aa03e
Camilo  Hernandez <camilo.hernandezmarin@jumbo.com>
2020-04-24 19:09:04 +0200

    some basic testing

---
+++ driver-tests/src/main/java/test/Main.java
@@ -0,0 +1,18 @@
+package test;
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (CqlSession session = CqlSession.builder().build()) {
+            ResultSet rs = session.execute("select * from killrvideo.videos_by_tag");
+            Row row = rs.one();
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+            System.out.println(row.getUuid("video_id"));
+            System.out.println(row.getString("title"));
+        }
+    }
+}
